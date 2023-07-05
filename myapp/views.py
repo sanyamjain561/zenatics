d1b16f1fe6ff62db8ed7ace81243dfee8797e860
Sanyam Jain <77605056+sanyamjain561@users.noreply.github.com>
2023-07-06 00:10:06 +0530

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Feature
+# Create your views here.
+def index(request):
+    features = Feature.objects.all()
+    return render(request,'index.html', {'features' : features})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        pass2 = request.POST['pass2']
+
+        if password == pass2:
+            if User.objects.filter(email = email).exists():
+                messages.info(request,'email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'username already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request,'password not same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request,'credentials invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+def posts(request,pk):
+    return render(request,'posts.html',{'pk':pk})
